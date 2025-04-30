0de7b14c3b1689c47ce3104a30d801e001eb8621
singyuKang <rkdtlscks123@naver.com>
2025-04-30 17:17:11 +0900

    refactor : 게시물 수정 업데이트 연결

--- src/features/post-management/ui/EditPostModal.tsx
+++ src/features/post-management/ui/EditPostModal.tsx
@@ -3,16 +3,18 @@ import { Button } from "@shared/ui"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@shared/ui/dialog"
 import { Input } from "@shared/ui/input/Input"
 import { Textarea } from "@shared/ui/textarea/TextArea"
-import { useState } from "react"
+import { useSelectedPostHook } from "../model/useSelectedPost"
+import { useMutationEditPostUpdate } from "../api/api"
 
 const EditPostModal = () => {
   const { openEditPost, closeModal } = useModal()
-
-  const [selectedPost, setSelectedPost] = useState([])
+  const { selectedPost, updateSelectedPost, resetPost } = useSelectedPostHook()
+  const { editPost } = useMutationEditPostUpdate()
 
   const handleUpdatePost = () => {
-    // TODO : Update Post
-    console.log("Update Post")
+    editPost(selectedPost)
+    resetPost()
+    closeModal("editPost")
   }
 
   return (
@@ -25,13 +27,13 @@ const EditPostModal = () => {
           <Input
             placeholder="제목"
             value={selectedPost?.title || ""}
-            onChange={(e) => setSelectedPost({ ...selectedPost, title: e.target.value })}
+            onChange={(e) => updateSelectedPost({ ...selectedPost, title: e.target.value })}
           />
           <Textarea
             rows={15}
             placeholder="내용"
             value={selectedPost?.body || ""}
-            onChange={(e) => setSelectedPost({ ...selectedPost, body: e.target.value })}
+            onChange={(e) => updateSelectedPost({ ...selectedPost, body: e.target.value })}
           />
           <Button onClick={handleUpdatePost}>게시물 업데이트</Button>
         </div>
